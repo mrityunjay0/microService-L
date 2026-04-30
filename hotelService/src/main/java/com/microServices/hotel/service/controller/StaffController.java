1e7fab8ebf83109a86155c7f767a73d50bae7c79
Mrityunjay Kumar <mrityunjaykmr.mk@gmail.com>
2026-04-30 08:25:15 +0530

    Gateway

---
+++ hotelService/src/main/java/com/microServices/hotel/service/controller/StaffController.java
@@ -0,0 +1,20 @@
+package com.microServices.hotel.service.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff")
+public class StaffController {
+
+    @GetMapping()
+    public ResponseEntity<List<String>> getAllStaff() {
+        List<String> staffList = Arrays.asList("John Doe", "Jane Smith", "Alice Johnson");
+        return ResponseEntity.ok(staffList);
+    }
+}
